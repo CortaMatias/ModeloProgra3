9264f06a7c231b7aea1f13c93531d51b4334353d
Matias <matiascorta99@gmail.com>
2023-05-11 18:07:27 -0300

    terminado

--- modelo_pp/backend/AltaUsuarioJSON.php
+++ modelo_pp/backend/AltaUsuarioJSON.php
@@ -2,7 +2,7 @@
  require_once("./clases/Usuario.php");
 
 
- $_accion = isset($_POST["opcion"]) ? $_POST["opcion"] : "sin accion"; 
+ $_accion = isset($_POST["opcion"]) ? $_POST["opcion"] : "sin accion";
  $_nombre = isset($_POST["nombre"]) ? $_POST["nombre"] : "sin nombre";
  $clave = isset($_POST["clave"]) ? $_POST["clave"] : "sin clave"; 
 $_correo = isset($_POST["correo"]) ? $_POST["correo"] : "sin correo"; 
@@ -11,5 +11,5 @@ $usuario =  new Usuario();
 $usuario->nombre = $_nombre;
 $usuario->clave = $clave;
 $usuario->correo = $_correo;
-var_dump($usuario->GuardarEnArchivo());
+echo json_encode($usuario->GuardarEnArchivo());
 ?>
--- modelo_pp/backend/clases/Usuario.php
+++ modelo_pp/backend/clases/Usuario.php
@@ -1,9 +1,13 @@
-<?php 
-require_once("./AccesoDatos.php");
-use PDO;
+<?php
+
+require_once("./clases/accesoDatos.php");
+require_once("./clases/IBM.php");
+
+
 use ejercicio_bd\AccesoDatos;
 
- class Usuario {
+class Usuario implements IBM
+{
     public int $id;
     public string $nombre;
     public string $correo;
@@ -12,74 +16,130 @@ use ejercicio_bd\AccesoDatos;
     public string $perfil;
 
 
-    public function toJSON(){
-       $alumno = array("nombre" =>$this->nombre,
-       "correo" => $this->correo,
-       "clave" => $this->clave
+    public function toJSON()
+    {
+        $alumno = array(
+            "nombre" => $this->nombre,
+            "correo" => $this->correo,
+            "clave" => $this->clave
         );
         return json_encode($alumno);
     }
 
-    public function GuardarEnArchivo() {
-		$retorno = false;	
-		
-			//ABRO EL ARCHIVO
-			$ar = fopen("./archivos/usuarios.json", "a");//A - append()
-			//ESCRIBO EN EL ARCHIVO CON FORMATO: CLAVE-VALOR_UNO-VALOR_DOS
-			$cant = fwrite($ar, $this->toJSON(). "\r\n");
-			if($cant > 0)$retorno =array("exito" => true, "mensaje" => "agregado");						
-			else{$retorno=array("exito" => false, "mensaje" => "ERROR");}
-			//CIERRO EL ARCHIVO
-			fclose($ar);
-		
-		return $retorno;
-	}
+    public function GuardarEnArchivo()
+    {
+        //ABRO EL ARCHIVO
+        $ar = fopen("./archivos/usuarios.json", "a"); //A - append()
+        //ESCRIBO EN EL ARCHIVO CON FORMATO: CLAVE-VALOR_UNO-VALOR_DOS
+        $cant = fwrite($ar, $this->toJSON() . "\r\n");
+        if ($cant > 0) $retorno = array("exito" => true, "mensaje" => "agregado");
+        else $retorno = array("exito" => false, "mensaje" => "ERROR");
+        
+        //CIERRO EL ARCHIVO
+        fclose($ar);
 
-    public static function TraerTodosJSON(){
-        $retorno = [];			
+        return $retorno;
+    }
+
+    public static function TraerTodosJSON()
+    {
+        $retorno = [];
         $ar = fopen("./archivos/usuarios.json", "r");
-		while(!feof($ar)){
+        while (!feof($ar)) {
             $linea = fgets($ar);
             $usuario_leido = json_decode($linea);
-            if(isset($usuario_leido)){
+            if (isset($usuario_leido)) {
                 $usuario = new Usuario();
                 $usuario->nombre = $usuario_leido->nombre;
                 $usuario->correo = $usuario_leido->correo;
                 $usuario->clave = $usuario_leido->clave;
                 array_push($retorno, $usuario);
             }
-        }		
-		return $retorno;
+        }
+        return $retorno;
     }
 
-    public function Agregar(){
-        
-            $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
-            
-            $consulta =$objetoAccesoDato->retornarConsulta("INSERT INTO usuarios (nombre, correo, clave,id_perfil)"
-                                                        . "VALUES(:nombre, :correo, :clave, :id_perfil)");
-            
-            $consulta->bindValue(':nombre', $this->nombre,  PDO::PARAM_STR);
-            $consulta->bindValue(':clave', $this->clave, PDO::PARAM_STR);
-            $consulta->bindValue(':id_perfil', $this->id_perfil,  PDO::PARAM_INT);    
-            return $consulta->execute();                 
+    public function Agregar()
+    {
+
+        $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
+
+        $consulta = $objetoAccesoDato->retornarConsulta("INSERT INTO usuarios (nombre, correo, clave,id_perfil)"
+            . "VALUES(:nombre, :correo, :clave, :id_perfil)");
+
+        $consulta->bindValue(':nombre', $this->nombre,  PDO::PARAM_STR);
+        $consulta->bindValue(':correo', $this->correo,  PDO::PARAM_STR);
+        $consulta->bindValue(':clave', strval($this->clave), PDO::PARAM_STR);
+        $consulta->bindValue(':id_perfil', $this->id_perfil,  PDO::PARAM_INT);
+        return $consulta->execute();
     }
 
 
     public static function TraerTodos()
-    {    
+    {
         $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
-        
-        $consulta = $objetoAccesoDato->retornarConsulta("SELECT * FROM usuarios");        
-        
+
+        $consulta = $objetoAccesoDato->retornarConsulta("SELECT * FROM usuarios");
+
         $consulta->execute();
-        
-        $consulta->setFetchMode(PDO::FETCH_INTO, new Usuario);                                               
+
+        $consulta->setFetchMode(PDO::FETCH_INTO, new Usuario);
 
         return $consulta;
     }
 
 
-}
+    public static function TraerUno($params)
+    {
+        $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
+        $objParams = json_decode($params);
 
-?>
+        $consulta = $objetoAccesoDato->retornarConsulta("SELECT * FROM usuarios WHERE clave = :clave AND correo = :correo");
+
+        $consulta->bindValue(':correo', $objParams->correo, PDO::PARAM_STR);
+        $consulta->bindValue(':clave', strval($objParams->clave), PDO::PARAM_STR);
+
+        $consulta->execute();
+
+        $consulta->setFetchMode(PDO::FETCH_INTO, new Usuario);
+
+        $alumno = $consulta->fetch();
+
+
+        if ($alumno instanceof Usuario) return $alumno;
+        else return null;
+    }
+
+    public function Modificar()
+    {
+        $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
+
+        $consulta = $objetoAccesoDato->retornarConsulta("UPDATE usuarios SET correo = :correo,id_perfil = :id_perfil , nombre = :nombre, clave = :clave WHERE id = :id");
+
+        $consulta->bindValue(':correo', $this->correo, PDO::PARAM_STR);
+        $consulta->bindValue(':nombre', $this->nombre, PDO::PARAM_STR);
+        $consulta->bindValue(':clave', strval($this->clave), PDO::PARAM_STR);
+        $consulta->bindValue(':id_perfil', $this->id_perfil, PDO::PARAM_INT);
+        $consulta->bindValue(':id', $this->id, PDO::PARAM_INT);
+
+        return $consulta->execute();
+    }
+
+
+    public static function Eliminar($id)
+    {
+        $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
+
+        $consulta = $objetoAccesoDato->retornarConsulta("DELETE FROM usuarios WHERE id = :id");
+
+        $consulta->bindValue(':id', $id, PDO::PARAM_INT);
+
+        $eliminado = $consulta->execute();
+
+        if ($eliminado && $consulta->rowCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
